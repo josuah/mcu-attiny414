760b65582ea99e7b40fe79954ccc35e866b970e3
Josuah Demangeon <me@josuah.net>
2022-09-27 23:27:25 +0200

    initial usart implementaiton

--- attiny414.h
+++ attiny414.h
@@ -1,7 +1,10 @@
 #include <stdint.h>
 
-#ifndef ATTINY104_H
-#define ATTINY104_H
+#ifndef ATTINY414_H
+#define ATTINY414_H
+
+#define PIN_USART_TX 1 // PORTA
+#define PIN_USART_RX 2 // PORTA
 
 #ifndef CLK_MAIN_KHZ
 #define CLK_MAIN_KHZ 16000
@@ -36,16 +39,22 @@ typedef struct {
 typedef struct {
     volatile uint8_t RXDATAL;           // 0x00
     volatile uint8_t RXDATAH;           // 0x01
+#define USART_RXDATAH_RXCIF             (0x01u << 7)
+#define USART_RXDATAH_BUFOVF            (0x01u << 6)
+#define USART_RXDATAH_FERR              (0x01u << 2)
+#define USART_RXDATAH_PERR              (0x01u << 1)
+#define USART_RXDATAH_DATA8             (0x01u << 0)
     volatile uint8_t TXDATAL;           // 0x02
     volatile uint8_t TXDATAH;           // 0x03
+#define USART_TXDATAH_DATA8             (0x01u << 0)
     volatile uint8_t STATUS;            // 0x04
 #define USART_STATUS_RXCIF              (0x01u << 7)
-#define USART_STATUS_TXCI               (0x01u << 6)
-#define USART_STATUS_DREI               (0x01u << 5)
-#define USART_STATUS_RXSI               (0x01u << 4)
-#define USART_STATUS_ISFI               (0x01u << 3)
-#define USART_STATUS_BD                 (0x01u << 1)
-#define USART_STATUS_WF                 (0x01u << 0)
+#define USART_STATUS_TXCIF              (0x01u << 6)
+#define USART_STATUS_DREIF              (0x01u << 5)
+#define USART_STATUS_RXSIF              (0x01u << 4)
+#define USART_STATUS_ISFIF              (0x01u << 3)
+#define USART_STATUS_BDF                (0x01u << 1)
+#define USART_STATUS_WFB                (0x01u << 0)
     volatile uint8_t CTRLA;             // 0x05
 #define USART_CTRLA_RXCIE               (0x01u << 7)
 #define USART_CTRLA_TXCIE               (0x01u << 6)
@@ -56,13 +65,13 @@ typedef struct {
 #define USART_CTRLA_RS485_Msk           (0x03u << 0)
 #define USART_CTRLA_RS485_Pos                     0
     volatile uint8_t CTRLB;             // 0x06
-#define USART_CTRLB_RXEN                (0x01u << 7)
-#define USART_CTRLB_TXEN                (0x01u << 6)
-#define USART_CTRLB_SFDEN               (0x01u << 4)
-#define USART_CTRLB_ODME                (0x01u << 3)
-#define USART_CTRLB_RXMODE_Msk          (0x01u << 1)
-#define USART_CTRLB_RXMODE_Pos                    1
-#define USART_CTRLB_MPCM                (0x01u << 0)
+#define USART_CTRLB_RXEN                (0x01 << 7)
+#define USART_CTRLB_TXEN                (0x01 << 6)
+#define USART_CTRLB_SFDEN               (0x01 << 4)
+#define USART_CTRLB_ODME                (0x01 << 3)
+#define USART_CTRLB_RXMODE_Msk          (0x03 << 1)
+#define USART_CTRLB_RXMODE_Pos                   1
+#define USART_CTRLB_MPCM                (0x01 << 0)
     volatile uint8_t CTRLC;             // 0x07
 #define USART_CTRLC_CMODE_Msk           (0x03u << 6)
 #define USART_CTRLC_CMODE_Pos                     6
@@ -71,8 +80,8 @@ typedef struct {
 #define USART_CTRLC_SBMODE              (0x01u << 3)
 #define USART_CTRLC_CHSIZE_Msk          (0x07u << 0)
 #define USART_CTRLC_CHSIZE_Pos                    0
-    volatile uint16_t BAUD;             // 0x08
-    volatile uint8_t RESERVED0[0x0A - 0x09];
+    volatile uint8_t BAUD;              // 0x08
+    volatile uint8_t RESERVED0[0x0B - 0x0A];
     volatile uint8_t DBGCTRL;           // 0x0B
 #define USART_DBGCTRL_DBGRUN            (0x01u << 0)
     volatile uint8_t EVCTRL;            // 0x0C
@@ -109,5 +118,73 @@ typedef struct {
     volatile uint8_t XOSC32KCTRLA;      // 0x1C
 } hw_clkctrl_t;
 #define CLKCTRL ((hw_clkctrl_t *)0x0060)
+
+typedef struct {
+    volatile uint8_t CTRLA;             // 0x00
+    volatile uint8_t CTRLB;             // 0x01
+    volatile uint8_t CTRLC;             // 0x02
+    volatile uint8_t CTRLD;             // 0x03
+    volatile uint8_t CTRLE;             // 0x04
+    volatile uint8_t RESERVED0[0x07 - 0x04];
+    volatile uint8_t EVCTRLA;           // 0x08
+    volatile uint8_t EVCTRLB;           // 0x09
+    volatile uint8_t RESERVED1[0x0B - 0x09];
+    volatile uint8_t INTCTRL;           // 0x0C
+    volatile uint8_t INTFLAGS;          // 0x0D
+    volatile uint8_t STATUS;            // 0x0E
+    volatile uint8_t RESERVED2[0x10 - 0x0F];
+    volatile uint8_t INPUTCTRLA;        // 0x10
+    volatile uint8_t INPUTCTRLB;        // 0x11
+    volatile uint8_t FAULTCTRL;         // 0x12
+    volatile uint8_t RESERVED[0x14 - 0x13];
+    volatile uint8_t DLYCTRL;           // 0x14
+    volatile uint8_t DLYVAL;            // 0x15
+    volatile uint8_t RESERVED3[0x18 - 0x16];
+    volatile uint8_t DITCTRL;           // 0x18
+    volatile uint8_t DITVAL;            // 0x19
+    volatile uint8_t RESERVED4[0x1E - 0x1A];
+    volatile uint8_t DBGCTRL;           // 0x1E
+    volatile uint8_t RESERVED5[0x22 - 0x1F];
+    volatile uint8_t CAPTUREA;          // 0x22
+    volatile uint8_t CAPTUREB;          // 0x24
+    volatile uint8_t RESERVED6[0x28 - 0x26];
+    volatile uint8_t CMPASET;           // 0x28
+    volatile uint8_t CMPACLR;           // 0x2A
+    volatile uint8_t CMPBSET;           // 0x2C
+    volatile uint8_t CMPBCLR;           // 0x2E
+} hw_tca_t;
+#define TCA0 ((hw_tca_t *)0x0A00)
+
+typedef struct {
+    volatile uint8_t CTRLA;             // 0x00
+#define TCB_CTRLA_RUNSTDBY              (0x01u << 6)
+#define TCB_CTRLA_SYNCUPD               (0x01u << 4)
+#define TCB_CTRLA_CLKSEL_Msk            (0x03u << 1)
+#define TCB_CTRLA_CLKSEL_Pos                      1
+#define TCB_CTRLA_ENABLE                (0x01u << 0)
+    volatile uint8_t CTRLB;             // 0x01
+#define TCB_CTRLB_ASYNC                 (0x01u << 6)
+#define TCB_CTRLB_CCMPINT               (0x01u << 5)
+#define TCB_CTRLB_CCMPEN                (0x01u << 4)
+#define TCB_CTRLB_CNTMODE_Msk           (0x07u << 0)
+#define TCB_CTRLB_CNTMODE_Pos                     0
+    volatile uint8_t RESERVED0[0x04 - 0x02];
+    volatile uint8_t EVCTRL;            // 0x04
+#define TCB_EVCTRL_FILTER               (0x01u << 6)
+#define TCB_EVCTRL_EDGE                 (0x01u << 4)
+#define TCB_EVCTRL_CAPTEI               (0x01u << 0)
+    volatile uint8_t INTCTRL;           // 0x05
+#define TCB_INTCTRL_CAPT                (0x01u << 0)
+    volatile uint8_t INTFLAGS;          // 0x06
+#define TCB_INTFLAGS_CAPT               (0x01u << 0)
+    volatile uint8_t STATUS;            // 0x07
+#define TCB_STATUS_RUN                  (0x01u << 0)
+    volatile uint8_t DBGCTRL;           // 0x08
+#define TCB_DBGCTRL_DBGRUN              (0x01u << 0)
+    volatile uint8_t TEMP;              // 0x09
+    volatile uint16_t CNT;              // 0x0A
+    volatile uint16_t CCMP;             // 0x0C
+} hw_tcb_t;
+#define TCB0 ((hw_tcb_t *)0x0A40)
 
 #endif
--- firmware.c
+++ firmware.c
@@ -4,14 +4,19 @@
 
 int main(void)
 {
-    // Test to export the clock.
-    CLKCTRL->MCLKCTRLA |= CLKCTRL_MCLKCTRLA_CLKOUT;
+    PORTA->DIRSET = 0xFF;
+    PORTA->OUTCLR = 0xFF;
 
-    __sei();
-    PORTA->DIRSET |= 1u << PIN;
-    for (;;) {
-        PORTA->OUTSET = 1u << PIN;
-        PORTA->OUTCLR = 1u << PIN;
-    }
+    // Usart
+    USART0->BAUD = 5;
+
+    // Counter
+    TCB0->CCMP = 1024;
+    TCB0->CNT = 1024;
+    TCB0->CTRLB |= TCB_CTRLB_CCMPEN;
+    TCB0->CTRLB &= TCB_CTRLB_CNTMODE_Msk;
+    TCB0->CTRLB |= 0x07 << TCB_CTRLB_CNTMODE_Pos;
+    TCB0->CTRLA |= TCB_CTRLA_ENABLE;
+
     return 0;
 }
