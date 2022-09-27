8a68b7465e0a3fbeca4b816f0f8ae43d241f6adb
Josuah Demangeon <me@josuah.net>
2022-09-28 00:36:46 +0200

    have the counter working, enables to measure the frequency of the clock

--- firmware.c
+++ firmware.c
@@ -1,22 +1,16 @@
 #include "attiny414.h"
 
-#define PIN 1
-
 int main(void)
 {
-    PORTA->DIRSET = 0xFF;
-    PORTA->OUTCLR = 0xFF;
+    PORTA->DIRSET = 1 << 5;
+    PORTA->OUTSET = 1 << 5;
 
-    // Usart
-    USART0->BAUD = 5;
-
-    // Counter
-    TCB0->CCMP = 1024;
-    TCB0->CNT = 1024;
+    TCB0->CCMP = 0x80FF;
+    TCB0->CTRLA |= TCB_CTRLA_ENABLE;
     TCB0->CTRLB |= TCB_CTRLB_CCMPEN;
     TCB0->CTRLB &= TCB_CTRLB_CNTMODE_Msk;
     TCB0->CTRLB |= 0x07 << TCB_CTRLB_CNTMODE_Pos;
-    TCB0->CTRLA |= TCB_CTRLA_ENABLE;
+    TCB0->CTRLB |= TCB_CTRLB_CCMPEN;
 
     return 0;
 }
